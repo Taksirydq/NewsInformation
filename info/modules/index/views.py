e83a091e560d0bc25e2cb99d92d5139c5001dacc
Taksirydq <1981438992@qq.com>
2018-10-03 13:15:43 +0800

    点击排行前后端逻辑实现

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,9 +1,10 @@
 from . import index_blu
 from flask import current_app, jsonify
-from info.models import User
+from info.models import User,News
 from flask import render_template
 from flask import session
 from info.utils.response_code import RET
+from info import constants
 
 
 # 　2.使用蓝图
@@ -36,11 +37,30 @@ def index():
         使用方式：data.user_info.id
     """
     # ---------------------获取新闻点击排行榜---------------------------------
+    try:
+        news_rank_list = News.query.order_by(News.clicks.desc()).limit(constants.CLICK_RANK_MAX_NEWS).all()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="查询新闻排行数据异常")
+    """
+        查询到的是对象列表
+       # news_rank_list 对象列表===》 [news1, news2, ...新闻对象 ]
+       # news_rank_dict_list 字典列表===> [{新闻字典}, {}, {}]
+    """
+    # 字典列表初始化
+    news_rank_dict_list = []
+    # 将新闻对象列表转换成字典列表
+    for news_obj in news_rank_list if news_rank_list else []:
+        # 将新闻对象转成字典
+        news_dict = news_obj.to_dict()
+        # 构建字典列表
+        news_rank_dict_list.append(news_dict)
 
     # 组织响应数据字典
     # 如果user存在,我就使用user转换成字典给到其user_info,如果user不存在，就用None替换user.to_dict()
     data = {
         "user_info": user.to_dict() if user else None,
+        "news_rank_list":news_rank_dict_list
     }
     return render_template("news/index.html", data=data)
 
