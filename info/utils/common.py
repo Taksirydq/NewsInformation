dafb54ec36aea49c1950196ba817767c3f02962d
Taksirydq <1981438992@qq.com>
2018-10-09 11:21:20 +0800

    捕获404异常，页面统一处理

--- info/utils/common.py
+++ info/utils/common.py
@@ -1,3 +1,8 @@
+# 自定义过滤器函数
+from flask import session, current_app, jsonify, g
+from info.utils.response_code import RET
+
+
 def do_index_class(index):
     """根据index下标返回对应的class值"""
     if index == 1:
@@ -8,3 +13,34 @@ def do_index_class(index):
         return "third"
     else:
         return ""
+
+
+import functools
+
+
+# 获取当前登录用户信息的装饰器
+def user_decorative_device(view_func):
+    # 使用装饰器改变了被装饰的函数的一些特性，函数名称，可以使用functools解决这个问题
+    @functools.wraps(view_func)
+    def wrapper(*args, **kwargs):
+        # 一.实现装饰器应该完成的功能(新添加的功能)
+        # 1. 获取当前用户登录的id
+        user_id = session.get("user_id")
+        user = None  # type:User
+        # 进行延迟导入，解决循环导入db的问题
+        from info.models import User
+        # 2.查询用户对象
+        if user_id:
+            try:
+                # 获取到用户对象
+                user = User.query.get(user_id)
+            except Exception as e:
+                current_app.logger.error(e)
+                return jsonify(errno=RET.DBERR, errmsg="查询用户对象异常")
+
+        # 3. 将用户对象保存到g对象中
+        g.user = user
+        # 二：实现原有函数的基本功能
+        result = view_func(*args, **kwargs)
+        return result
+    return wrapper
